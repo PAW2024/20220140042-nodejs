6f93e96a31a34ffff84086da9c956afb822039b8
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-17 13:00:41 +0700

    todo.js dan postman

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const todoRoutes = require('./routes/todo.js');
 const port = 4000;
+
+app.use(express.json());
+
+
+app.use('/todos', todoRoutes);
 
 app.set('view engine', 'ejs');
 
