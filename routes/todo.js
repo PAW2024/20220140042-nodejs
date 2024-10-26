b4f0311306ca05919e8bf6ce1528208f2560c850
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-26 08:22:37 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -10,8 +10,10 @@ let todos = [
   },
 ];
 
+//Endpoint untuk menndapatkan data
 router.get('/',(req, res) => {res.json(todos); });
 
+//Endpoint untuk menambahkan tugas
 router.post('/', (req, res) => {
   const newTodo = {
     id: todos.length + 1,
@@ -22,6 +24,25 @@ router.post('/', (req, res) => {
   res.status(201).json(newTodo);
 });
 
-
-
 module.exports = router;
+
+//Endpoint untuk menghapus tugas
+router.delete('/:id', (req, res) => {
+  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
+
+  const deletedTodo = todos.splice(todoIndex, 1)[0]; //menghapus dan menyimpan todo yang dihapus
+  res.status(200).json({message:`Tugas'${deletedTodo.task}'telah dihapus`})
+});
+
+//Endpoint untuk memperbarui tugas
+router.put('/:id', (req, res) => {
+  const todo = todos.find(t => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
+  todo.task = req.body.task || todo.task;
+
+  res.status(200).json({
+    message: `Tugas dengan ID ${todo.id} telah diperbarui`,
+    updatedTodo: todo
+  })
+})
